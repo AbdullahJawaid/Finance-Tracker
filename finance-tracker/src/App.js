4b6ca0b638dfaf46a774d230a98ebffe258b0bdf
AbdullahJawaid <91978361+AbdullahJawaid@users.noreply.github.com>
2022-11-13 22:25:48 +0500

    first commit

---
+++ finance-tracker/src/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
+
+// page components'
+import Navbar from './components/Navbar'
+import Home from './pages/home/Home'
+import Signup from './pages/signup/Signup'
+import Login from './pages/login/Login'
+
+
+
+function App() {
+  const { authIsReady, user } = useAuthContext()
+
+  return (
+    <div className="App">
+     {authIsReady && (
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+         <Route path="/" element={user ? <Home /> : <Navigate to="/login" />}></Route>
+
+            <Route path='/login' element={!user ? <Login /> : <Navigate to='/' />}></Route>
+
+            <Route path='/signup' element={!user ? <Signup /> : <Navigate to='/' />}></Route>
+      </Routes>
+    </BrowserRouter>
+     )}
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ finance-tracker/src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBjAdI1ySF0IH8DA_S4ObnFhIcx9W0Y49w",
+    authDomain: "mymoney-5aa70.firebaseapp.com",
+    projectId: "mymoney-5aa70",
+    storageBucket: "mymoney-5aa70.appspot.com",
+    messagingSenderId: "10657185586",
+    appId: "1:10657185586:web:e30c0e464a34c0af36212b"
+  };
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth=firebase.auth()
+
+export { projectFirestore,projectAuth }
